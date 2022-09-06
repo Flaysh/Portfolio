8aa62fbd8ccf482bf7caef70b498c9adbbed9369
Flaysh <flaysh@pm.me>
2022-09-06 18:54:36 +0300

    gitignore and gh-pages

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,7 +2,7 @@ import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
     name: 'Itay Flaysher',
-    title: "*Open for freelance work!",
+    title: "Open for freelance work!",
     desciption:"Co-Founder and Software Engineer at MoonWiz.",
     image: 'https://i.imgur.com/R5mBcNM.jpg',
     resumePdf: resume

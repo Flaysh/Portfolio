1352660a61a072945ac1e7bc8a026812a10f75f0
Flaysh MB <Flaysh@moonwiz.io>
2022-09-07 13:46:00 +0300

    liter pic

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Itay Flaysher',
     title: "Open for freelance work!",
     desciption:"Co-Founder and Software Engineer at MoonWiz.",
-    image: 'https://i.imgur.com/R5mBcNM.jpg',
+    image: 'https://i.imgur.com/iyyjoyV.jpg',
     // resumePdf: resume
 }
